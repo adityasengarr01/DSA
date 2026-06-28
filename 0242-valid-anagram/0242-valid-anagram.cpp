1e380e662a24749117a4d79c098417feb52483a0
aditya-code <a20800190@gmail.com>
2026-06-28 09:23:32 +0530

    Time: 0 ms (100%), Space: 9.5 MB (92.54%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,12 +1,29 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.length() != t.length()) {
+        if(s.size() != t.size()){
             return false;
         }
 
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s == t;
+        int freq[26] = {0};
+        int frequ[26] = {0};
+
+        for(int i=0;i<s.size();i++){
+            freq[s[i] - 'a']++;
+
+        }
+
+        for(int j=0;j<t.size();j++){
+            frequ[t[j] - 'a']++;
+        }
+          for(int k=0;k<26;k++){
+
+
+          
+        if(freq[k] != frequ[k]){
+            return false;;
+        }
+          }
+        return true;
     }
 };
