85713a6cd94bb48e1dd7b6276f7070b214af23df
aditya-code <a20800190@gmail.com>
2026-07-31 10:18:34 +0530

    Time: 0 ms (100%), Space: 9.4 MB (72.82%) - LeetHub

---
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+
+        while (true) {
+
+            int index = s.find(part);
+
+            if (index == string::npos) {
+                break;
+            }
+
+            s.erase(index, part.length());
+        }
+
+        return s;
+    }
+};
