697119df4ffa6a5b4531e9a294e5391ae667196b
aditya-code <a20800190@gmail.com>
2026-08-01 01:41:05 +0530

    Time: 4 ms (85.97%), Space: 25.3 MB (92.36%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+
+        int freq[26] = {0};
+
+        for (char ch : word) {
+            freq[ch - 'a']++;
+        }
+        sort(freq, freq + 26, greater<int>());
+
+        int ans = 0;
+
+        for (int i = 0; i < 26; i++) {
+
+            if (freq[i] == 0)
+                break;
+
+            int pushes = (i / 8) + 1;
+
+            ans += freq[i] * pushes;
+        }
+
+        return ans;
+    }
+};
